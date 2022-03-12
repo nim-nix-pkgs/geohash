8f3b72066477dd95dae8a4917c70e6e0b4787384
hugosenari <hugosenari@gmail.com>
2022-03-12 02:54:55 -0300

    chore: re index geohash

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim implementation of the geohash latitude/longitude geocode system'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."geohash-master".url = "path:./master";
+  inputs."geohash-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."geohash-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
